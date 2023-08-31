ff35de987fcaaf2c1773ba81c6021fba30f2e1dc
RauenLeGrange <rauenleg@gmail.com>
2023-08-31 13:19:42 +1200

    Camera follow player
    camera now follows player movement

---
+++ COMP602 Fishing Chronicles/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float xOffset= 0, yOffset = 5, zOffset = -8;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x + xOffset, player.transform.position.y + yOffset, player.transform.position.z + zOffset);
+    }
+}
