b0b5b4cd2792e54d8137a17b17a993cdf7cfcaff
RauenLeGrange <rauenleg@gmail.com>
2023-08-31 15:16:19 +1200

    XP bar working
    Xp bar scripts added

--- COMP602 Fishing Chronicles/Assets/Scripts/XP.cs
+++ COMP602 Fishing Chronicles/Assets/Scripts/XP.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class XP : MonoBehaviour
 {
+    public int currentXP;
+    public int MaxXP;
+    public int level = 1;
+
+    public SliderScript slider;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
+        currentXP = 0;
+        MaxXP = level * 100;
+
+        slider.SetSliderMax(MaxXP);
+        slider.SetSlider(currentXP);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            getXP(10);
+        }
+    }
+
+    public void getXP(int xp)
+    {
+        currentXP += xp;
+        slider.SetSlider(currentXP);
+
+        if(currentXP>= MaxXP)
+        {
+            level++;
+            MaxXP = level * 100;
+            slider.SetSliderMax(MaxXP);
+            currentXP = 0;
+            slider.SetSlider(currentXP);
+        }
     }
 }
