d3b2c213255b8ead3cfe2b91927334eac49b37cc
Yellowbunbunz <brooklynsponge@gmail.com>
2023-09-03 17:40:27 +1200

    Done
    Added everything. Basically done. May need tiny things to make it look better later.

--- COMP602 Fishing Chronicles/Assets/Scripts/DayNightManager.cs
+++ COMP602 Fishing Chronicles/Assets/Scripts/DayNightManager.cs
@@ -6,42 +6,71 @@ using UnityEngine.UI;
 
 public class DayNightManager : MonoBehaviour
 {
-    // Variable set ups
-    public TMP_Text timeText;        // To create a reference to the text
-    private float currentTime = 0f;     // Variable for the current time
-    private int inGameTime = 0;         // Variable for the current time but in game
+    // Reference variables to the TextMeshPro texts
+    public TMP_Text timeText;           
+    public TMP_Text dayText;
 
-    private const float cycleDuration = 1440f;       // Constant variable for 12 minutes in seconds (1 ingame day)
-    private const float secondsToGameHours = 60f;   // Constant variable for how many real seconds to create 1 ingame day
+    // Varaibles for storing time and days
+    private float currentTime = 0f;     
+    private int inGameTime = 0;         
+    private int inGameDay = 1;
+
+    // Constant varaibles for the time calculations
+    private const float cycleDuration = 1440f;       // 12 minutes in seconds (1 ingame day)
+    private const float secondsToGameHours = 60f;    // How many real seconds to create 1 ingame hour
+
 
     void Start()
     {
-        inGameTime = Mathf.FloorToInt(currentTime / secondsToGameHours) + 6;
-        UpdateTimeUI();
+        // Initialising the ingame time and day and startup
+        inGameTime = Mathf.FloorToInt(currentTime / secondsToGameHours) + 6;    // ChatGPT helped me with this math
+        inGameDay = 1;
+
+        // Updating the UI with the initial time and day
+        UpdateTimeUI();     
     }
 
     void Update()
     {
+        // Updating the current time with real time seconds
         currentTime += Time.deltaTime;
 
-        int newTime = Mathf.FloorToInt(currentTime / secondsToGameHours) + 6;
+        // Calculating the new ingame time
+        int newTime = Mathf.FloorToInt(currentTime / secondsToGameHours) + 6;   // ChatGPT helped me with this math too 
+
+        // Checking if the ingame time has changed
         if (newTime != inGameTime)
         {
+            // If it has, update the ingame time and the UI
             inGameTime = newTime;
             UpdateTimeUI();
         }
 
+        // Checking if a full ingame day has passed
         if(currentTime >= cycleDuration)
         {
+            // Resetting the time and start time to how it was initialised as
             currentTime = 0f;
             inGameTime = 6;
+
+            // Increment the day by 1
+            inGameDay++;
+
+            // Update the UI
             UpdateTimeUI();
         }
     }
 
+    // This method updates the UI elements when called
     private void UpdateTimeUI()
     {
+        // Put the time within a 24 hour time format
         int displayTime = (inGameTime % 24);
+
+        // Updates the time text within the format of Hour:Minute
         timeText.text = string.Format("{0:00}:00", displayTime);
+
+        // Updates the day text with the current ingame day
+        dayText.text = string.Format("{0}", inGameDay);
     }
 }
