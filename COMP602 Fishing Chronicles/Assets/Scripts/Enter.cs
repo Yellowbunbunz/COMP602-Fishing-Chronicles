878511128e4e13d02e5f6fdf0b08e06c29ed5b8c
KyleRandomnes <kylejleg@gmail.com>
2023-09-21 23:35:03 +1200

    Scenes
    Can now change scenes between the 3 main area's now adding game mode

--- COMP602 Fishing Chronicles/Assets/Scripts/Enter.cs
+++ COMP602 Fishing Chronicles/Assets/Scripts/Enter.cs
@@ -10,9 +10,10 @@ public class Enter : MonoBehaviour
         //This holds tags that are safe to change scenes to, so to change to scene House use the tag "house".
         //Now I'm using an array and looping though it, so I can easily just add 1 line to the array and it
         //will now work for the new scene.
-        string[] safetags = { "Shop1", "SampleScene", "House" };
+        string[] safetags = { "Shop1", "MainWorld", "House" };
         for(int i = 0; safetags.Length > i; i++)
         {
+            Debug.Log(collision.collider.tag);
             if (collision.collider.tag == safetags[i])
             {
                 SceneManager.LoadScene(collision.collider.tag);//loads the scene
