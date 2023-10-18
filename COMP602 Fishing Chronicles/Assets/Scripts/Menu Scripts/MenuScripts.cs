ad714239a344e3f0e74ef2ce7ef55048f2354316
KyleRandomnes <kylejleg@gmail.com>
2023-10-18 15:35:22 +1300

    Menu made

---
+++ COMP602 Fishing Chronicles/Assets/Scripts/Menu Scripts/MenuScripts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScripts : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainWorld");//loads the scene
+    }
+
+    public void Quit()
+    {
+        Debug.Log("exited app");
+        Application.Quit();
+    }
+}
