ee15aae91343390278fbf8fe7ab88103dcf9b7b3
KyleRandomnes <kylejleg@gmail.com>
2023-10-19 11:16:27 +1300

    Fixed dock

--- COMP602 Fishing Chronicles/Assets/Scripts/MainWorldScripts/EnterFishingGame.cs
+++ COMP602 Fishing Chronicles/Assets/Scripts/MainWorldScripts/EnterFishingGame.cs
@@ -7,21 +7,18 @@ public class EnterFishingGame : MonoBehaviour
 {
     bool fish = false;
 
-    private void Start()
-    {
-        Debug.Log(fish);
-    }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.collider.tag == "Player")
+        if (collision.collider.tag == "Player")
         {
             fish = true;
         }
+
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if(collision.collider.tag == "Player")
+        if (collision.collider.tag == "Player")
         {
             fish = false;
         }
@@ -29,7 +26,7 @@ public class EnterFishingGame : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.F))
+        if (Input.GetKey(KeyCode.F) && fish)
         {
             SceneManager.LoadScene("FishingScene");//loads the scene
         }
